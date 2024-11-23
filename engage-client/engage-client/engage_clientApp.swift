1e5cb4d33fd3b040c6f5482621cd79eeba8115d8
NikolaiMadlener <nikolai@madlener.io>
2024-11-23 14:39:36 +0100

    init client

---
+++ engage-client/engage-client/engage_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  engage_clientApp.swift
+//  engage-client
+//
+//  Created by Nikolai Madlener on 23.11.24.
+//
+
+import SwiftUI
+
+@main
+struct engage_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
